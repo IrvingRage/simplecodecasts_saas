3217fa5e6b1ed0bba0d26c1c71542a9ea4de89e4
Luis Cano <luiscanoa2008@gmail.com>
2016-04-20 02:02:50 +0000

    Added MAiler ad View

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -7,8 +7,15 @@ class ContactsController < ApplicationController
         @contact = Contact.new(contact_params)
         
         if @contact.save
+            name = params[:contact][:name]
+            email = params[:contact][:email]
+            body = params [:contact][:comments]
+            
+            ContactMailer.contact_email(name,email,body).deliver
+            
             flash[:success] = "Message Sent!"
             redirect_to new_contact_path
+            
         else
             flash[:error] = "Error!"
             redirect_to new_contact_path
